b6c30ec0f4629777ced42588d7530cce44c6da2d
myself <myselfmjs@vip.qq.com>
2018-11-20 15:56:56 +0800

    springboot 跳转页面

--- src/main/java/spring/boot/sample/DemoApplication.java
+++ src/main/java/spring/boot/sample/DemoApplication.java
@@ -1,10 +1,43 @@
 package spring.boot.sample;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.web.ErrorPageFilter;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
-@SpringBootApplication
-public class DemoApplication {
+import javax.servlet.DispatcherType;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages="spring.boot.sample")
+@EnableScheduling
+public class DemoApplication extends SpringBootServletInitializer {
+
+	@Bean
+	public ErrorPageFilter errorPageFilter() {
+		return new ErrorPageFilter();
+	}
+
+	@Bean
+	public FilterRegistrationBean disableSpringBootErrorFilter(ErrorPageFilter filter) {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(filter);
+		filterRegistrationBean.setEnabled(false);
+		return filterRegistrationBean;
+	}
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
---
+++ src/main/java/spring/boot/sample/control/LoginController.java
@@ -0,0 +1,27 @@
+package spring.boot.sample.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: majunsheng
+ * @Date: 2018/11/20
+ **/
+@Controller
+@RequestMapping("login")
+public class LoginController {
+
+    @RequestMapping(value = "json",produces = "application/json; charset=utf-8")
+    @ResponseBody
+    public String hello() {
+        return "Hello Spring-Boot";
+    }
+
+    @RequestMapping(value = "init",method = RequestMethod.GET)
+    public String init(){
+
+        return "/WEB-INF/jsp/success.jsp";
+    }
+}
